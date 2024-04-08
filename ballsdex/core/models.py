5c88377b7fc5709ea1bb421f4d9bc7473977718a
Kris Kaelep <kris.kaelep@gmail.com>
2024-04-08 19:55:42 +0300

    some await

--- ballsdex/core/models.py
+++ ballsdex/core/models.py
@@ -301,8 +301,8 @@ class BallInstance(models.Model):
         trade_content = ""
 
         await self.fetch_related("trade_player", "special")
-        all_balls: List[Ball] = sorted(balls.values(), key=lambda ball: ball.rarity)
-        index = next((i for i, ball in enumerate(all_balls) if ball == self.ball), None)
+        all_balls: List[Ball] = await sorted(balls.values(), key=lambda ball: ball.rarity)
+        index = await next((i for i, ball in enumerate(all_balls) if ball == self.ball), None)
         log.info(self.ball)
         log.info(balls.values())
         log.info(all_balls)
